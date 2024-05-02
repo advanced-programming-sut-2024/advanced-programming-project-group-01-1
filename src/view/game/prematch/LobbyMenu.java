5ec98017eb1c38ecde150d275a37fd594a782066
S2speed <sepehrssm@gmail.com>
2024-05-02 16:33:54 +0330

    added LobbyMenu methods

--- src/view/game/prematch/LobbyMenu.java
+++ src/view/game/prematch/LobbyMenu.java
@@ -1,14 +1,46 @@
 package view.game.prematch;
 
+import model.Result;
 import view.Menuable;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 
 public class LobbyMenu implements Menuable {
 
 	@Override
 	public void run(Scanner scanner) {
 
+	}
+
+	private Result selectFaction(Matcher matcher){
+		// TODO:
+		return null;
+	}
+
+	private Result saveDeck(Matcher matcher) {
+		// TODO:
+		return null;
+	}
+
+	private Result loadDeck(Matcher matcher){
+		// TODO:
+		return null;
+	}
+
+	private Result selectLeader(Matcher matcher){
+		// TODO:
+		return null;
+	}
+
+	private Result addToDeck(Matcher matcher){
+		// TODO:
+		return null;
+	}
+
+	private Result deleteFromDeck(Matcher matcher){
+		// TODO:
+		return null;
 	}
 
 }
