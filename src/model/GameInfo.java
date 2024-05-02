d551f7ec8736b4dfb73ee74aa7e04be920b39678
m.khavari2005 <m.khavari2005@gmail.com>
2024-05-02 15:47:43 +0330

    added GameInfo and User id

---
+++ src/model/GameInfo.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GameInfo implements Serializable {
+
+	private int opponentId;
+ 	private Date date;
+	private ArrayList<Integer> opponentScores, myScores;
+	private int opponentFinalScore, myFinalScore;
+	private int winnerId;
+
+	public GameInfo(User opponent, ArrayList<Integer> opponentScores, ArrayList<Integer> myScores, int opponentFinalScore, int myFinalScore, User winner) {
+		this.opponentId = opponent.getId();
+		this.date = new Date();
+		this.opponentScores = opponentScores;
+		this.myScores = myScores;
+		this.opponentFinalScore = opponentFinalScore;
+		this.myFinalScore = myFinalScore;
+		this.winnerId = winner.getId();
+	}
+
+	public int getOpponentId() {
+		return opponentId;
+	}
+
+	public Date getDate() {
+		return new Date(String.valueOf(this.date));
+	}
+
+	public ArrayList<Integer> getOpponentScores() {
+		return new ArrayList<>(this.opponentScores);
+	}
+
+	public ArrayList<Integer> getMyScores() {
+		return new ArrayList<>(this.myScores);
+	}
+
+	public int getOpponentFinalScore() {
+		return this.opponentFinalScore;
+	}
+
+	public int getMyFinalScore() {
+		return this.myFinalScore;
+	}
+
+	public int getWinnerId() {
+		return this.winnerId;
+	}
+}
--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,10 @@
 package model;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable {
+
+	private final int id;
 	private String username;
 	private String nickname;
 	private String password;
@@ -9,19 +12,85 @@ public class User {
 	private Question question;
 
 	public User(String username, String nickname, String password, String email, Question question) {
+		this.id = User.getNumberOfUsers() + 1;
 		this.username = username;
 		this.nickname = nickname;
 		this.password = password;
 		this.email = email;
 		this.question = question;
+		this.updateData();
 	}
 
-	public static User getCurrentUser() { //TODO
+	public int getId() {
+		return this.id;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public String getNickname() {
+		return this.nickname;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public Question getQuestion() {
+		return this.question;
+	}
+
+	public int getMaxScore() {
+		// TODO:
+		return 0;
+	}
+
+	public int getRank() {
+		// TODO:
+		return 0;
+	}
+
+	public int getNumberOfPlayedMatches() {
+		// TODO:
+		return 0;
+	}
+
+	public int getNumberOfWins() {
+		// TODO:
+		return 0;
+	}
+
+	public int getNumberOfDraws() {
+		// TODO:
+		return 0;
+	}
+
+	public int getNumberOfLosses() {
+		// TODO:
+		return 0;
+	}
+
+	private void updateData() {
+		// TODO:
+	}
+
+	public static User getCurrentUser() {
+		// TODO:
 		return null;
 	}
 
-	public static User getUserByUsername() { //TODO
+	public static User getUserByUsername() {
+		// TODO:
 		return null;
 	}
 
+	public static int getNumberOfUsers() {
+		// TODO:
+		return 0;
+	}
 }
