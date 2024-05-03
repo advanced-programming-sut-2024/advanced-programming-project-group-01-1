eef6ca261e8a4d300f9c2af31a94c393cb4bf96c
amirvahidi <amirvahidi83@gmail.com>
2024-05-03 15:53:37 +0330

    add horn ability

--- src/model/card/unit/Unit.java
+++ src/model/card/unit/Unit.java
@@ -6,6 +6,42 @@ public abstract class Unit extends Card {
 
 	final int basePower;
 	final String faction;
+	int hornCount = 0;
+	int boostCount = 0;
+	int multiplier = 1;
+	boolean debuff = false;
+
+	public int getHornCount() {
+		return this.hornCount;
+	}
+
+	public void setHornCount(int hornCount) {
+		this.hornCount = hornCount;
+	}
+
+	public int getBoostCount() {
+		return this.boostCount;
+	}
+
+	public void setBoostCount(int boostCount) {
+		this.boostCount = boostCount;
+	}
+
+	public int getMultiplier() {
+		return this.multiplier;
+	}
+
+	public void setMultiplier(int multiplier) {
+		this.multiplier = multiplier;
+	}
+
+	public boolean isDebuff() {
+		return this.debuff;
+	}
+
+	public void setDebuff(boolean debuff) {
+		this.debuff = debuff;
+	}
 
 	public Unit(String name, String faction, int basePower) {
 		super(name);
---
+++ src/model/card/unit/melee/HornMelee.java
@@ -0,0 +1,21 @@
+package model.card.unit.melee;
+
+import model.card.ability.Horn;
+
+public class HornMelee extends Melee implements Horn{
+
+	public HornMelee(String name, String faction, int basePower) {
+		//TODO: implement the constructor
+		super(name, faction, basePower);
+	}
+
+	@Override
+	public void put(int row) throws Exception {
+		//TODO: implement the put method
+	}
+
+	@Override
+	public void pull() throws Exception {
+		//TODO: implement the pull method
+	}
+}
