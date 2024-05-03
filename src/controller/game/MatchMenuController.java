2434ace5d47e1f4162cb89fba8b7d2af0eca5c5e
S2speed <sepehrssm@gmail.com>
2024-05-03 23:13:13 +0330

    added GameMenu and GameMenuController

---
+++ src/controller/game/MatchMenuController.java
@@ -0,0 +1,87 @@
+package controller.game;
+
+import model.Result;
+
+public class MatchMenuController {
+
+	public static Result vetoCard(int cardNumber) {
+		// TODO:
+		return null;
+	}
+
+	public static Result showHand(int number) {
+		// TODO:
+		return null;
+	}
+
+	public static Result remainingInDeck() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showDiscordPile() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showRow(int rowNumber) {
+		// TODO:
+		return null;
+	}
+
+	public static Result showWeatherSystem() {
+		// TODO:
+		return null;
+	}
+
+	public static Result placeCard(int cardNumber, int rowNumber) {
+		// TODO:
+		return null;
+	}
+
+	public static Result showLeader() {
+		// TODO:
+		return null;
+	}
+
+	public static Result useLeaderAbility() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showPlayerInfo() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showPlayerLives() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showHandSize() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showTurnInfo() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showTotalPower() {
+		// TODO:
+		return null;
+	}
+
+	public static Result showRowPower() {
+		// TODO:
+		return null;
+	}
+
+	public static Result passTurn() {
+		// TODO:
+		return null;
+	}
+
+}
--- src/view/game/GameMenu.java
+++ src/view/game/GameMenu.java
@@ -1,14 +1,36 @@
 package view.game;
 
+import model.Result;
 import view.Menuable;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 
 public class GameMenu implements Menuable {
 
 	@Override
 	public void run(Scanner scanner) {
 
+	}
+
+	private Result vetoCard(Matcher matcher) {
+		// TODO:
+		return null;
+	}
+
+	private Result showHand(Matcher matcher) {
+		// TODO:
+		return null;
+	}
+
+	private Result showRow(Matcher matcher) {
+		// TODO:
+		return null;
+	}
+
+	private Result placeCard(Matcher matcher) {
+		// TODO:
+		return null;
 	}
 
 }
