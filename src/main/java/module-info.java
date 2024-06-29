b010e6c045f91484174bb6c71f08716868ca9fd8
voidXD <amirvahidi83@gmail.com>
2024-06-29 17:24:28 +0330

    javaFX dependencies added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module w {
-	opens model to com.google.gson, com.fasterxml.jackson.databind;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.media;
+	opens model to javafx.fxml, com.google.gson, com.fasterxml.jackson.databind;
 	opens model.user to com.fasterxml.jackson.databind, com.google.gson;
 	requires com.google.gson;
 }
