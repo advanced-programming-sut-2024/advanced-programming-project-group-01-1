9067daff210b5fc90842a1cf48a020c73079d971
S2speed <sepehrssm@gmail.com>
2024-07-01 18:00:26 +0330

    implemented login menu controller methods

--- src/main/java/controller/sign/LoginMenusController.java
+++ src/main/java/controller/sign/LoginMenusController.java
@@ -1,36 +1,51 @@
 package controller.sign;
 
+import controller.enums.Validation;
 import model.Result;
+import model.user.User;
+import view.Appview;
+import view.sign.register.RegisterMenu;
 
 public class LoginMenusController {
 
 	public static Result login(String username, String password, boolean stayLoggedIn) {
-		//TODO:
-		return null;
+		User user = User.getUserByUsername(username);
+		if (user == null) return new Result("Username doesn't exist", false);
+		if (!user.getPassword().equals(password)) return new Result("Password is incorrect", false);
+		User.setLoggedInUser(user);
+		return new Result("Login successful", true);
 	}
 
 	public static Result forgotPassword(String username) {
-		//TODO:
-		return null;
+		User user = User.getUserByUsername(username);
+		if (user == null) return new Result("Username doesn't exist", false);
+		User.setLoggedInUser(user);
+		return new Result(user.getQuestion().getQuestion(), true);
 	}
 
 	public static Result answerQuestion(int questionNumber, String answer) {
-		// TODO:
-		return null;
+		User user = User.getLoggedInUser();
+		if (user.getQuestion().isAnswerCorrect(answer))
+			return new Result("Answer is correct\nSet a new password", true);
+		return new Result("Answer is incorrect", false);
 	}
 
 	public static Result setPassword(String password) {
-		// TODO:
-		return null;
+		if (!Validation.STRONG_PASSWORD.matches(password)) return new Result("Password is weak", false);
+		User user = User.getLoggedInUser();
+		user.setPassword(password);
+		return new Result("Password set successfully", true);
 	}
 
 	public static Result exit() {
-		// TODO:
-		return null;
+		System.exit(0);
+		return new Result("Exiting", true);
 	}
 
 	public static Result goToRegisterMenu() {
-		// TODO:
-		return null;
+		User.setLoggedInUser(null);
+		Appview.setMenu(new RegisterMenu());
+		Appview.runMenu();
+		return new Result("Going to register menu", true);
 	}
 }
