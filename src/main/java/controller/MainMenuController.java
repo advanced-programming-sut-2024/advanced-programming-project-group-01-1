84e79646cd4bf09cc3888b146e048313cdb83b4a
m.khavari2005 <m.khavari2005@gmail.com>
2024-07-01 22:47:07 +0330

    implement MainMenuController methods

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,22 +1,29 @@
 package controller;
 
 import model.Result;
+import view.Appview;
+import view.game.prematch.MatchFinderMenu;
+import view.sign.login.LoginMenu;
+import view.user.ProfileMenu;
 
 public class MainMenuController {
 
 	public static Result logout() {
-		// TODO:
-		return null;
+		Appview.setMenu(new LoginMenu());
+		Appview.runMenu();
+		return new Result("Logged out successfully", true);
 	}
 
 	public static Result goToProfileMenu() {
-		// TODO:
-		return null;
+		Appview.setMenu(new ProfileMenu());
+		Appview.runMenu();
+		return new Result("Entered Profile Menu", true);
 	}
 
 	public static Result goToMatchFinderMenu() {
-		// TODO:
-		return null;
+		Appview.setMenu(new MatchFinderMenu());
+		Appview.runMenu();
+		return new Result("Entered Match Finder Menu", true);
 	}
 
 }
